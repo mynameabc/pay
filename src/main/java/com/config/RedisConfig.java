585f21631e45356fc584b2559e3991d5fb61b666
mynameabc <21654428@qq.com>
2020-01-06 00:32:36 +0800

    -

---
+++ src/main/java/com/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean(destroyMethod="shutdown")
+    RedissonClient redis(@Value("classpath:/redis-config.yml") Resource configFile) throws IOException {
+        Config config = Config.fromYAML(configFile.getInputStream());
+        return Redisson.create(config);
+    }
+}
